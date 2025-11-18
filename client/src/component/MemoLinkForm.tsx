c72683fd0f4a99414e19046e6a817820c3591c8a
lunasky-hy <19776220+lunasky-hy@users.noreply.github.com>
2025-11-18 19:35:55 +0900

    antd form action using post data

--- client/src/component/MemoLinkForm.tsx
+++ client/src/component/MemoLinkForm.tsx
@@ -1,6 +1,5 @@
 import { LinkOutlined, SendOutlined } from '@ant-design/icons';
 import { Button, Card, Form, Input, Space } from 'antd';
-import { useReducer } from 'react';
 import useSWRMutation from 'swr/mutation';
 
 type MemoLinkField = {
@@ -11,11 +10,6 @@ type MemoLinkField = {
 const initForm: MemoLinkField = {
   linkUri: '',
   memo: '',
-};
-
-type FormReducerAction = {
-  name: string;
-  value: string;
 };
 
 const postRequest = (url: string, { arg }: { arg: MemoLinkField }) => {
@@ -30,52 +24,33 @@ const postRequest = (url: string, { arg }: { arg: MemoLinkField }) => {
     .catch((e) => console.error(e));
 };
 
-const formReducer = (
-  prevStatus: MemoLinkField,
-  { name, value }: FormReducerAction
-) => {
-  switch (name) {
-    case 'clear':
-      console.log('クリアされました。');
-      return { linkUri: '', memo: '' };
-    case 'linkUri':
-    case 'memo':
-      return { ...prevStatus, [name]: value };
-    default:
-      throw new Error('undefined target');
-  }
-};
-
-const validationForm = (data: MemoLinkField) => {
-  if (!data.linkUri) return false;
-  try {
-    new URL(data.linkUri);
-  } catch {
-    return false;
-  }
-  return true;
-};
-
 export default function MemoLinkField() {
-  const [memolink, dispatch] = useReducer(formReducer, initForm);
+  const [form] = Form.useForm();
   const { trigger, isMutating } = useSWRMutation('/api/memolink', postRequest);
 
-  const isValidFormData = validationForm(memolink);
-  const onSubmit = () =>
-    trigger(memolink).then(() => dispatch({ name: 'clear', value: '' }));
-
-  console.log(memolink);
+  const onSubmit = (values: MemoLinkField) =>
+    trigger(values).then(form.resetFields);
 
   return (
     <Card style={{ padding: '1em' }}>
       <h2 style={{ marginBottom: '1em' }}>リンクをメモ</h2>
-      <Form
+      <Form<MemoLinkField>
+        form={form}
         layout="vertical"
         autoComplete="off"
         initialValues={{ ...initForm }}
         onFinish={onSubmit}
       >
-        <Form.Item name={'linkUri'} label="リンクURL">
+        <Form.Item<MemoLinkField>
+          name="linkUri"
+          label="リンクURL"
+          rules={[
+            {
+              required: true,
+              message: '記録したいリンク先URLを追加してください。',
+            },
+          ]}
+        >
           <Space.Compact style={{ width: '100%' }}>
             <Space.Addon>
               <LinkOutlined />
@@ -84,22 +59,14 @@ export default function MemoLinkField() {
               name="linkUri"
               placeholder="リンクを貼り付け..."
               allowClear
-              value={memolink.linkUri}
-              onChange={(e) =>
-                dispatch({ name: e.target.name, value: e.target.value })
-              }
             />
           </Space.Compact>
         </Form.Item>
-        <Form.Item name={'memo'} label="メモ・感想">
+        <Form.Item<MemoLinkField> name="memo" label="メモ・感想">
           <Input.TextArea
             name="memo"
             placeholder="読んだ時の気持ちを残しましょう..."
             autoSize={{ minRows: 2, maxRows: 4 }}
-            value={memolink.memo}
-            onChange={(e) =>
-              dispatch({ name: e.target.name, value: e.target.value })
-            }
             allowClear
             showCount
             maxLength={255}
@@ -110,7 +77,6 @@ export default function MemoLinkField() {
           htmlType="submit"
           style={{ width: 'max-content' }}
           loading={isMutating}
-          disabled={!isValidFormData}
         >
           <SendOutlined />
           登録
