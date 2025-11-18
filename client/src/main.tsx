2de00cdd852faeefba0700912af6a0ff9fb55250
lunasky-hy <19776220+lunasky-hy@users.noreply.github.com>
2025-11-18 10:47:05 +0900

    append react router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import './global.css';
+import List from '@root/src/root/List';
 
-createRoot(document.getElementById('root')!).render(
+const root = document.getElementById('root')!;
+
+ReactDOM.createRoot(root).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<List />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
