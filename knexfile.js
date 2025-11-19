79210af9c4ed839f3e01180bfac5d5a9c4fe396a
lunasky-hy <19776220+lunasky-hy@users.noreply.github.com>
2025-11-19 11:23:40 +0900

    change knexfile setting

--- knexfile.js
+++ knexfile.js
@@ -41,16 +41,14 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
+      connectionString: process.env.DATABASE_CONNECTION,
     },
     pool: {
       min: 2,
       max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations',
+      directory: './db/migrations',
     },
   },
 };
