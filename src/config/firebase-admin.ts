dee245e6c157bae47663592b2d36c1c39f25d3d2
lunasky-hy <19776220+lunasky-hy@users.noreply.github.com>
2025-11-19 21:32:12 +0900

    firebase service account setting

--- src/config/firebase-admin.ts
+++ src/config/firebase-admin.ts
@@ -1,14 +1,19 @@
 import admin, { ServiceAccount } from 'firebase-admin';
-import serviceAccount from './serviceAccountKey.json';
 
-// const serviceAccount = {
-//   type: 'service_account',
-//   project_id: process.env.FIREBASE_PROJECT_ID,
-//   private_key:
-//     process.env.FIREBASE_PRIVATE_KEY &&
-//     process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-//   client_email: process.env.FIREBASE_CLIENT_EMAIL,
-// };
+const serviceAccount = {
+  type: process.env.FIREBASE_APP_TYPE,
+  project_id: process.env.FIREBASE_APP_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_APP_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_APP_PRIVATE_KEY_ID?.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_APP_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_APP_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_APP_AUTH_URI,
+  token_uri: process.env.FIREBASE_APP_TOKEN_URI,
+  auth_provider_x509_cert_url:
+    process.env.FIREBASE_APP_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_APP_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_APP_UNIVERSE_DOMAIN,
+};
 
 try {
   if (!admin.apps.length) {
